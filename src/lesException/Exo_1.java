423a8b564a2fed4058863e0a17f16b7deb9b3a60
Murcyado <noah.caroux@gmail.com>
2023-02-13 15:13:32 +0100

    exo 1 exception correction

--- src/lesException/Exo_1.java
+++ src/lesException/Exo_1.java
@@ -6,22 +6,29 @@ public class Exo_1 {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	    int a, b, resultat;
-	      
-	    System.out.print("Entrez le premier nombre : ");
-	    a = sc.nextInt();
-	    System.out.print("Entrez le deuxième nombre : ");
-	    b = sc.nextInt();
+	    int x, y;
+	    double resultat;  
+	    System.out.print("Entrez le premier entier : ");
+	    x = sc.nextInt();
+	    System.out.print("Entrez le deuxième entier : ");
+	    y = sc.nextInt();
 	      
 	    try {
-	       resultat = a / b;
-	       System.out.println("Le résultat de la division est : " + resultat);
+	       resultat = x / y;
+	       System.out.println("Le résultat est : " + resultat);
 	    } 
 	    catch (ArithmeticException e) {
-	         System.out.println("Impossible de diviser par 0");
+	        System.out.println("Impossible de diviser par 0" + e.getMessage());
+	        e.printStackTrace();
+			return;
 	    } 
 	    finally {
+	    	if(x != 0 || y!=0) {
 	         System.out.println("La division est correcte");
+	    	}
+	    	else {
+	    		System.out.println("Division impossible");
+	    	}
 	    }
 	}
 }

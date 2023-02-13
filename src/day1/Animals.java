b63a8f94ef8b5dfb2d33fba6035b50bf6893ddb2
Murcyado <noah.caroux@gmail.com>
2023-02-13 10:33:38 +0100

    attributs instances constructeurs

--- src/day1/Animals.java
+++ src/day1/Animals.java
@@ -1,5 +1,23 @@
 package day1;
 
 public class Animals {
-
+	// Attributs d'instance
+	String nom;
+	int age;
+	
+	//Constructeurs
+	
+	public Animals() 
+	{
+		System.out.println("Construction d'un Animal");
+		
+	}
+	
+	
+	public Animals(String nom, int age) 
+	{
+		System.out.println("Construction d'un Animal avec 2 paramètres");
+		this.nom = nom;
+		this.age = age;
+	}
 }
--- src/day1/Application.java
+++ src/day1/Application.java
@@ -5,6 +5,17 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java");
+		//Instances
+		Animals a1 = new Animals();// appel au constructeur par défaut
+		
+		Animals a2 = new Animals();// appel au constructeur par défaut
+		
+		System.out.println(a1.nom); // null
+		System.out.println(a1.age); // 0
+		
+		Animals a3 = new Animals("Rex", 3);
+		System.out.println(a3.nom);
+		System.out.println(a3.age); 
 	}
 
 }

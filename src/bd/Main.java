fe97925ec4311e47174dfd7b9b22833d55926951
Murcyado <noah.caroux@gmail.com>
2023-02-14 09:39:21 +0100

    Affichage de toutes les data de la table étudiant

--- src/bd/Main.java
+++ src/bd/Main.java
@@ -7,63 +7,78 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-
 public class Main {
+	
+	public static void getData(Statement stmt) throws SQLException
+	{
+		ResultSet rs = null;
+		String rq = "select * from etudiant";
+	     
+	     rs = stmt.executeQuery(rq);
+	     
+	     while(rs.next()) {
+	    	 
+	    	 int id = rs.getInt("id");
+	    	 String nom = rs.getString("nom");
+	    	 String email = rs.getString("email");
+	    	 int age= rs.getInt("age");
+	    	 
+	    	 System.out.println("ID : "+id+" | Nom : "+nom+" | Email : "+email+" | Age : "+age);
+	     }
+	}
 
-	public static void main(String[] args) throws SQLException {
+	
+	public static int insertData(Statement stmt) throws SQLException
+	{
+		 Scanner sc = new Scanner(System.in);
+	     
+	     System.out.println("Donner votre nom");
+	     String nom=sc.nextLine();
+	     
+	     System.out.println("Donner votre email");
+	     String email=sc.nextLine();
+	     
+	     System.out.println("Donner votre age");
+	     int age=sc.nextInt();
+	     
+	     //1) Insert dans la table Etudiant
+	      
+	     String sql ="insert into etudiant (nom,email,age) values('"+nom+"','"+email+"','"+age+"')";
+	     int res = stmt.executeUpdate(sql);
+	     //System.out.println(res);
+	     return res;
+	}
+	public static void main(String[] args)  throws Exception{
 		Connection connection = null;
+		Statement stmt = null;
+		//ResultSet rs = null;
 		
 		 try {
 	     
 	      Class.forName("com.mysql.jdbc.Driver");
 	            
-	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit",
-	                "root", "");
+	     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecoleit","root", "");
+	     //System.out.println(connection);
+	      stmt = connection.createStatement();
 	     
-	     Statement stmt = connection.createStatement();
-	     /*
-	     Scanner sc = new Scanner(System.in);
-	     System.out.println("Donner le nom : ");
-	     String nom =sc.nextLine();
+		     //1)Lecture de data depuis le clavier
+		      insertData(stmt);
+		     //2)Affichage de toutes les data de la table étudiant
+		     getData(stmt);
 	     
-	     System.out.println("Donner l'email : ");
-	     String email =sc.nextLine();
 	     
-	     System.out.println("Donner votre age : ");
-	     int age = sc.nextInt();
-	     
-	     String sql="insert into etudiant (nom,email,age) values('"+nom+"','"+email+"','"+age+"')";
-	     int res = stmt.executeUpdate(sql);
-	     
-	     System.out.println(res);
-	     System.out.println(connection);
-	     System.out.println("Connécté");
-	     */
-	     
-	     // Affichage de toutes les data de la table étudiant
-	     String rq = "select * from etudiant";
-	     ResultSet rs = stmt.executeQuery(rq);
-	     while(rs.next())
-	     {
-	    	 int id = rs.getInt("id");
-	    	 String nom = rs.getString("nom");
-	    	 String email = rs.getString("email");
-	    	 int age = rs.getInt("age");
-	    	 
-	    	 System.out.println("ID : "+id+" Nom : "+nom+" Mail : "+email+" age : "+age);
-	     }
-	     
+	     //System.out.println(connection);
 		 }
 		 catch(Exception exception) {
-			 System.out.println(exception.getMessage());
-		 }
-		 finally {
-			 if(connection!=null) {
-				 connection.close();
-			 }
+			 System.out.println("Exception : "+ exception.getMessage());
 		 }
 		 
-		 System.out.println("Fin du programme");
+		 finally {
+			 if(connection!=null)
+				 connection.close();
+		 }
+		 
+		 System.out.println("Fin du programme...");
 
 	}
 
